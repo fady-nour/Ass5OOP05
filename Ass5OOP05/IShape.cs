cec54467365d9902ba36aeb963af0f9984d8c390
Fadynour <fadynour194@gmail.com>
2025-08-10 18:19:26 +0300

    Add project files.

---
+++ Ass5OOP05/IShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass5OOP05
+{
+    internal interface IShape
+    {        double Area { get; }
+         void DisplayShapeInfo();
+        
+    }
+    internal interface ICircle:IShape
+    {        
+        
+    }
+    internal interface IRectangle :IShape
+    {     
+
+    }
+}
---
+++ Ass5OOP05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass5OOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
