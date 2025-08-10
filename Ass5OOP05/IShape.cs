c43f81af2d086aafa7d7d8e2036732ffe89fa524
Fadynour <fadynour194@gmail.com>
2025-08-10 19:18:50 +0300

    Q1

--- Ass5OOP05/IShape.cs
+++ Ass5OOP05/IShape.cs
@@ -6,17 +6,54 @@ using System.Threading.Tasks;
 
 namespace Ass5OOP05
 {
+    #region Q1
     internal interface IShape
-    {        double Area { get; }
-         void DisplayShapeInfo();
-        
-    }
-    internal interface ICircle:IShape
-    {        
-        
-    }
-    internal interface IRectangle :IShape
-    {     
+    { double Area { get; }
+        void DisplayShapeInfo();
 
     }
+    internal interface ICircle : IShape
+    {
+        double Radius { get; set; }
+    }
+    internal interface IRectangle : IShape
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+
+    }
+    public class Circle : ICircle
+    {
+        public double Radius { get; set; }
+        public double Area => Math.PI * Radius * Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Circle: Radius = {Radius}, Area = {Area:F2}");
+        }
+    }
+
+    public class Rectangle : IRectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Area => Width * Height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Rectangle: Width = {Width}, Height = {Height}, Area = {Area:F2}");
+        }
+    }
+    #endregion
 }
